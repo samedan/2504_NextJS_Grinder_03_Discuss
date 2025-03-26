d262da3733b1385166613be8415d4b9ac6107d79
John Doe <johndoe@example.com>
2025-03-26 10:35:19 +0100

    Post Creation Form validation

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,50 @@
 "use server";
 
-export async function createPost() {
+import type { Post } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    console.log("NOT Success");
+    return { errors: result.error.flatten().fieldErrors };
+  }
+
+  // Errors AUTh
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  return { errors: {} };
   // TO DO: revalidate topic show page
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -13,28 +13,43 @@ import * as actions from "@/actions";
 import FormButton from "@/components/common/form-button";
 
 export default function PostCreateForm() {
+  const [formState, action, isPending] = useActionState(actions.createPost, {
+    errors: {},
+  });
+
+  console.log(formState.errors);
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form>
-          <div className="flex-col gap-4 w-80 flex ">
+        <form action={action}>
+          <div className="flex-col gap-4 w-80 flex p-2">
             <h3 className="text-lg">Create a Post</h3>
             <Input
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
               name="title"
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
             />
-            <Input
+            <Textarea
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
             />
-            <FormButton isLoading={}>Create Post</FormButton>
+            {/* // Auth Errors */}
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form.join(", ")}
+              </div>
+            ) : null}
+            <FormButton isLoading={isPending}>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
